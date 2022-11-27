25e86fb03bb847c01749f4f1c0a9252a17c00b5a
Kamran Tajerbashi <kamrantajerbashi@gmail.com>
2022-11-27 22:15:05 +0330

    Project layout Build on Home Controller and index2 & about2 View on _Layer1 in Views Folder

---
+++ Pro-1-MVC Learning/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Pro_1_MVC_Learning.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pro_1_MVC_Learning.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ActionResult Index()
+        {
+            // Index
+
+            var person = new Person();
+            person.Id = 1;
+            person.Name = "Kaihan";
+            person.LastName = "Tajerbashi";
+            person.Age = 30;
+            person.Phone = "09020320844";
+
+            return View(person);
+        }
+        public ActionResult Index1()
+        {
+            // Index1
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            // _Layer1
+
+            var person = new Person();
+            person.Id = 1;
+            person.Name = "Kaihan";
+            person.LastName = "Tajerbashi";
+            person.Age = 30;
+            person.Phone = "09020320844";
+
+            return View(person);
+        }
+        public ActionResult About2()
+        {
+            // _Layer1
+            return View();
+        }
+    }
+}
--- Pro-1-MVC Learning/Models/DB_Class.cs
+++ Pro-1-MVC Learning/Models/DB_Class.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace Pro_1_MVC_Learning.Models
 {
-    public class DB_Class
+    public class DB_Class:DbContext
     {
+        public DB_Class():base("DB") { }
+        public DbSet<Person> People { get; set; }
     }
 }
