adad90f0ea6a273830807a45a96c128261e41406
Kamran Tajerbashi <kamrantajerbashi@gmail.com>
2022-12-05 14:33:17 +0330

    Machine Key Classes is Added With 2 type sample and has key in MachinKey

--- Pro-1-MVC Learning/Areas/Admin/Controllers/HomeController.cs
+++ Pro-1-MVC Learning/Areas/Admin/Controllers/HomeController.cs
@@ -1,17 +1,25 @@
-﻿using System;
+﻿using Pro_1_MVC_Learning.Models.Function_Codes;
+using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using System.Web.WebPages;
 
 namespace Pro_1_MVC_Learning.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Admin/Home
-        public ActionResult Index()
+        public ActionResult Index(string word)
         {
-            return View();
+            if (word.IsEmpty())
+            {
+                word = "Kaihan";
+            }
+            return View(model : MachineKeys.KeyEncode(word,"MyKey"));
         }
     }
 }
---
+++ Pro-1-MVC Learning/Models/Function Codes/MachineKeys.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pro_1_MVC_Learning.Models.Function_Codes
+{
+    public static class MachineKeys
+    {
+        public static string SampleEncode(string word)
+        {
+            var encrypt = System.Web.Security.MachineKey.Protect(System.Text.Encoding.UTF8.GetBytes(word));
+            return Convert.ToBase64String(encrypt);
+        }
+        public static string KeyEncode(string word,string key)
+        {
+            var encrypt = System.Web.Security.MachineKey.Protect(System.Text.Encoding.UTF8.GetBytes(word), key);
+            return Convert.ToBase64String(encrypt);
+        }
+        public static string SampleDecode(string word)
+        {
+            string decrypt = System.Text.Encoding.UTF8.GetString(System.Web.Security.MachineKey.Unprotect(Convert.FromBase64String(word)));
+            return decrypt;
+        }
+        public static string KeyDecode(string word, string key)
+        {
+            string decrypt = System.Text.Encoding.UTF8.GetString(System.Web.Security.MachineKey.Unprotect(Convert.FromBase64String(word), key));
+            return decrypt;
+        }
+    }
+}
