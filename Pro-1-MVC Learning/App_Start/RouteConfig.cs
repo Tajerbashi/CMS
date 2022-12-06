c266ec33d101743a01fa7a2f1d6b29dcf2e97393
Kamran Tajerbashi <kamrantajerbashi@gmail.com>
2022-12-07 00:13:05 +0330

    Upload Files and show file list in the div3 and can download each file with Download link next the files name

--- Pro-1-MVC Learning/App_Start/RouteConfig.cs
+++ Pro-1-MVC Learning/App_Start/RouteConfig.cs
@@ -21,7 +21,7 @@ namespace Pro_1_MVC_Learning
             routes.MapRoute("HomeAbout", "About", new { controller = "Home", action = "About" }, namespaces: new[] { "Pro_1_MVC_Learning.Controllers" });
             routes.MapRoute("HomePanel", "Panel", new { controller = "Home", action = "Panel" }, namespaces: new[] { "Pro_1_MVC_Learning.Controllers" });
             routes.MapRoute("HomeLogin", "Login", new { controller = "Home", action = "Login" }, namespaces: new[] { "Pro_1_MVC_Learning.Controllers" });
-            routes.MapRoute("FileIndex", "FileIndex", new { controller = "File", action = "Index" }, namespaces: new[] { "Pro_1_MVC_Learning.Controllers" });
+            routes.MapRoute("FileIndex", "File/Index", new { controller = "File", action = "Index" }, namespaces: new[] { "Pro_1_MVC_Learning.Controllers" });
             routes.MapRoute("FileRead", "FileRead", new { controller = "File", action = "Read" }, namespaces: new[] { "Pro_1_MVC_Learning.Controllers" });
             routes.MapRoute("FileDownload", "FileDownload", new { controller = "File", action = "Download" }, namespaces: new[] { "Pro_1_MVC_Learning.Controllers" });
             routes.MapRoute("FileUpload", "FileUpload", new { controller = "File", action = "Upload" }, namespaces: new[] { "Pro_1_MVC_Learning.Controllers" });
--- Pro-1-MVC Learning/Controllers/FileController.cs
+++ Pro-1-MVC Learning/Controllers/FileController.cs
@@ -1,34 +1,47 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 namespace Pro_1_MVC_Learning.Controllers
 {
     public class FileController : Controller
     {
+        
         // GET: File
         public ActionResult Index()
         {
-            return View();
+            var rootFolder = Server.MapPath("~/Files");
+            var files = System.IO.Directory.GetFiles(rootFolder).ToList();
+            return View(files);
         }
         public ActionResult Read()
         {
+            List<string> UploadedFiles = new List<string>();
             var fileFolder = Server.MapPath("~/Files");
             var fileContent = System.IO.File.ReadAllText(System.IO.Path.Combine(fileFolder, "FileTest1.txt"));
-            return View("Index",model : fileContent);
+            UploadedFiles.Add(fileContent);
+            return View("Index",UploadedFiles);
         }
 
-        public ActionResult Download()
+        public ActionResult Download(string fileName)
         {
             //FileContentResult
             //FilePathResult
             //FileStreamResult
+
             //return new FileContentResult(System.IO.File.ReadAllBytes(""));
+
             var fileFolder = Server.MapPath("~/Files");
-            var fileContent = System.IO.Path.Combine(fileFolder, "FileTest1.txt");
+            var fileContent = System.IO.Path.Combine(fileFolder, fileName);
+            if ( !System.IO.File.Exists(fileContent) )
+            {
+                return HttpNotFound();
+            }
             //return new FileContentResult(System.IO.File.ReadAllBytes(fileContent),System.Net.Mime.MediaTypeNames.Text.Plain);
             //return new FileContentResult(System.IO.File.ReadAllBytes(fileContent),System.Net.Mime.MediaTypeNames.Application.Octet);
             //return File(System.IO.File.ReadAllBytes(fileContent),System.Net.Mime.MediaTypeNames.Application.Octet,"FileName.txt");
-            return File(fileContent,System.Net.Mime.MediaTypeNames.Application.Octet,"FileName.txt");
+            return File(fileContent, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
             //return null;
         }
         
@@ -41,7 +54,8 @@ namespace Pro_1_MVC_Learning.Controllers
             var filePath = System.IO.Path.Combine(filesFolder,file.FileName);
             file.SaveAs(filePath);
             ViewBag.Message = "File Uploaded Successfully";
-            return View("Index");
+            return RedirectToAction("Index");
         }
+
     }
 }
