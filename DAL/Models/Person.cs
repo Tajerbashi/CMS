bd0fa640bf92e5d805d736db3fcb21e06479d225
Kamran Tajerbashi <kamrantajerbashi@gmail.com>
2022-12-10 03:06:12 +0330

    header navbar  section asidbar body and footer is designed

---
+++ DAL/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+    }
+}
--- Pro-1-MVC Learning/App_Start/RouteConfig.cs
+++ Pro-1-MVC Learning/App_Start/RouteConfig.cs
@@ -14,15 +14,16 @@ namespace Pro_1_MVC_Learning
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
+            routes.MapRoute( 
+                name: "Default", 
+                url: "{controller}/{action}/{id}", 
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
                 namespaces: new[] { "Pro_1_MVC_Learning.Controllers" }
                 );
-            routes.MapRoute("IndexHome", "Home/Index", new { controller = "Home", action = "Index", id = UrlParameter.Optional });
-            routes.MapRoute("IndexProducts", "Home/Products", new { controller = "Home", action = "Products", id = UrlParameter.Optional });
-            routes.MapRoute("IndexNews", "Home/News", new { controller = "Home", action = "News", id = UrlParameter.Optional });
+
+            //routes.MapRoute("IndexHome", "Home/Index", new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+            //routes.MapRoute("IndexProducts", "Home/Products", new { controller = "Home", action = "Products", id = UrlParameter.Optional });
+            //routes.MapRoute("IndexNews", "Home/News", new { controller = "Home", action = "News", id = UrlParameter.Optional });
         }
     }
 }
